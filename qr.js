22895d6579250456e62872191733bf7ed7511f1e
R-HUB760 <freerahim760@gmail.com>
2023-02-25 00:22:56 +0600

    nuyhiuiuydfwfddgdf

--- qr.js
+++ qr.js
@@ -67,8 +67,8 @@ async function qr_code_generator(url) {
         <h5> Max_Temperature: ${data.max_temp} </h5>
         <h5> Wind Speed: ${data.wind_speed} </h5>
         <h5> Wind Degrees: ${data.wind_degrees} </h5>
-        <h5> Sunrise: ${sunrise}</h5>
-        <h5> Sunset: ${sunset} </h5> <hr>
+        <h5 class="sun"> Sunrise: ${sunrise}</h5>
+        <h5 class="sun"> Sunset: ${sunset} </h5> <hr>
       `;
 
         // <h5>sunrise: ${sunRiseHours}:${sunRiseMinutes} AM</h5>
